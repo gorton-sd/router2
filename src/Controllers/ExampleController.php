16c670e85461722b570313900cb56c205510fc1d
Gorton Software Development <133211967+gorton-sd@users.noreply.github.com>
2025-08-20 16:58:00 +1000

    Create ExampleController.php

---
+++ src/Controllers/ExampleController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * ExampleController
+ * @url /example
+ */
+class ExampleController {
+    public function get() {
+        echo "GET: Hello from ExampleController!";
+    }
+
+    public function post() {
+        echo "POST: You posted to ExampleController!";
+    }
+}
